91605634c5baa1ef7ab93ff92252fd82f745d4c5
projectsouk <projectsouk@gmail.com>
2025-08-01 18:07:12 +0000

    Changes before Firebase Studio auto-run

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,3 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # ... other URL patterns
+]
 """
 URL configuration for mysite project.
 
